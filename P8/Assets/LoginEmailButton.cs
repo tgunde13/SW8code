7e7c02538da75931abe55fc32ef60bd8cf8cb295
tgunde13 <tgunde13@student.aau.dk>
2017-02-20 15:45:41 +0100

    Print test on click

---
+++ P8/Assets/LoginEmailButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginEmailButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public void onClick() {
+		Debug.Log ("Email Button Click");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
