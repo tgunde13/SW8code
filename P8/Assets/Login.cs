819a7a084a70f0daa158cc17a6570445d9f78ec2
tgunde13 <tgunde13@student.aau.dk>
2017-02-20 13:35:59 +0100

    Firebase Auth, Database, Analysis

--- P8/Assets/Login.cs
+++ P8/Assets/Login.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class Login : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		FirebaseApp.DefaultInstance..SetEditorDatabaseUrl("https://p8-server.firebaseio.com/");
+
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+		reference.Child ("players").Push().SetValueAsync ("test");
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
