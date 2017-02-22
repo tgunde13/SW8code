1ca64ea90bc4814068d3737719eb077e8abec74c
tgunde13 <tgunde13@student.aau.dk>
2017-02-22 10:58:48 +0100

    Alert dialog

--- P8/Assets/CreateAvatarEmail.cs
+++ P8/Assets/CreateAvatarEmail.cs
@@ -8,33 +8,30 @@ using Firebase.Unity.Editor;
 
 public class CreateAvatarEmail : MonoBehaviour {
 	public Text emailText, passwordText;
+	public ShowError ShowErrorScript;
 
 	// Use this for initialization
 	void Start () {
 	}
 
 	public void perform() {
-
-		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://p8-server.firebaseio.com/");
-
 		FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
 
-
-		Debug.Log ("test");
-
-		auth.CreateUserWithEmailAndPasswordAsync("2@tobiasgundersen.dk", "123456").ContinueWith(task => {
+		auth.CreateUserWithEmailAndPasswordAsync(emailText.text, passwordText.text).ContinueWith(task => {
 			if (task.IsCanceled) {
 				Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
 				return;
 			}
 			if (task.IsFaulted) {
 				Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+				ShowErrorScript.perform("An error occured. Possible reasons:\nThe e-mail address is already in use by another player");
 				return;
 			}
 
 			// Firebase user has been created.
 			Firebase.Auth.FirebaseUser newUser = task.Result;
 			Debug.LogFormat("Firebase user created successfully: {0} ({1})", newUser.DisplayName, newUser.UserId);
+			ShowErrorScript.perform("User created");
 		});
 	}
 	
---
+++ P8/Assets/ShowError.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowError : MonoBehaviour {
+	public Text messageField;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void perform(string message) {
+		messageField.text = message;
+		gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
