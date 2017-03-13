546226e02c5cb19bfeb3cf809a00eacfa814f42e
Archimedez <covese13@student.aau.dk>
2017-03-13 13:58:11 +0100

    Added packages and i18n manager and script

---
+++ P8/Assets/I18nManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mgl;
+
+public class I18nManager : MonoBehaviour {
+
+	private I18n i18n = I18n.Instance;
+
+	private void Start(){
+		SetLanguage();
+		DontDestroyOnLoad (gameObject);
+	}
+
+	private void SetLanguage(){
+		if (Application.systemLanguage == SystemLanguage.English) {
+			I18n.SetLocale ("en");
+		} else if (Application.systemLanguage == SystemLanguage.Danish) {
+			I18n.SetLocale ("da");
+		} else {
+			I18n.SetLocale ("en");
+		}
+
+
+	}
+
+	public string GetLocalisedString(string key){
+		return i18n.__ (key);
+	}
+}
---
+++ P8/Assets/LocalizeText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mgl;
+
+public class LocalizeText : MonoBehaviour {
+	
+	public string localizeKey;
+
+	void Start(){
+		GameObject gameObject = GameObject.Find ("I18nManager");
+		if (gameObject != null) {
+			GetComponent<Text> ().text = gameObject.GetComponent<I18nManager> ().GetLocalisedString (localizeKey);
+		} else {
+			GetComponent<Text> ().text = localizeKey;
+			Debug.Log("I18n: Couldn't find I18nManager");	
+		}
+	}
+
+
+}
