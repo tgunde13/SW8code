6e5409ce669ff99be1e2da509a2ba75a161d87be
Søren Larsen <the-mew@hotmail.com>
2017-04-11 16:09:25 +0200

    Added list of sprites and methods for removing

--- P8/Assets/SpriteController.cs
+++ P8/Assets/SpriteController.cs
@@ -4,14 +4,42 @@ using UnityEngine;
 
 public class SpriteController : MonoBehaviour {
 	public GameObject spawn_sprite;
+	public float y_pos_of_sprites = 490f;
+
+	private List<GameObject> sprites;
+
 	// Use this for initialization
 	void Start () {
-		addSprite (0, 0, "");
+		addSprite (0, 0, "placeholder");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	/// <summary>
+	/// Removes all sprites on the map.
+	/// </summary>
+	public void removeAllSprites(){
+		foreach (GameObject g in sprites) {
+			Destroy (g);
+		}
+	}
+
+	/// <summary>
+	/// Removes the sprite in the location provided.
+	/// </summary>
+	/// <param name="latitude">Latitude of sprite.</param>
+	/// <param name="longitude">Longitude of sprite.</param>
+	public void removeSprite(double latitude, double longitude){
+		Vector3 pos = new Vector3 ((float)latitude, (float)longitude, y_pos_of_sprites);
+		foreach (GameObject g in sprites) {
+			if (g.transform.position == pos) {
+				Destroy (g);
+				break;
+			}
+		}
 	}
 
 	/// <summary>
@@ -25,12 +53,25 @@ public class SpriteController : MonoBehaviour {
 	}
 
 
-	public void addSprite(Vector2 pos, string minion){
+	void addSprite(Vector2 pos, string minion){
 		Vector3 unity_pos = Mapbox.Scripts.Utilities.VectorExtensions.AsUnityPosition (pos);
-		unity_pos.y = 490;
-		GameObject sprite;
-		sprite = Instantiate (spawn_sprite);
+		unity_pos.y = y_pos_of_sprites;
+		GameObject sprite = minionTypeInstantiate(minion);
 		sprite.transform.name = ("Sprite - " + unity_pos.x + " | " + unity_pos.z);
 		sprite.transform.position = unity_pos;
+		sprites.Add (sprite);
+	}
+
+	/// <summary>
+	/// Instantiates the minion sprite to the given type.
+	/// </summary>
+	/// <returns>The type instantiate.</returns>
+	/// <param name="minion">Name of minion wanted.</param>
+	GameObject minionTypeInstantiate(string minion){
+		switch (minion) {
+		default:
+			Debug.Log ("SpriteController: " + minion + " sprite not found");
+			return Instantiate (spawn_sprite);
+		}
 	}
 }
