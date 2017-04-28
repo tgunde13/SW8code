d3465727d87ecd7492d4437bbef66b9b89718860
Søren Larsen <the-mew@hotmail.com>
2017-04-28 11:40:37 +0200

    WIP: battle

---
+++ P8/Assets/FindNPCMinion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindNPCMinion : MonoBehaviour {
+	public bool IsEnviormentBattle = false;
+	public Squad ComputerSquad;
+	public string UserKey = FirebaseAuthHandler.getUserId ();
+
+	private GameObject ComputerGameobject;
+	//FirebaseAuthHandler.getUserId ();
+	// Use this for initialization
+	void Start () {
+		ComputerGameobject = GameObject.FindGameObjectWithTag ("Minion");
+		if(ComputerGameobject != null){
+			ComputerSquad = ComputerGameobject.GetComponent<SpriteOnClick>().squad;
+			IsEnviormentBattle = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
