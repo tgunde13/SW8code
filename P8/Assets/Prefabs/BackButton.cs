ad094680f47ae91c1010b3be33f4651e25b7bac9
tgunde13 <tgunde13@student.aau.dk>
2017-05-08 15:00:05 +0200

    Whitespace patch for back button

--- P8/Assets/Prefabs/BackButton.cs
+++ P8/Assets/Prefabs/BackButton.cs
@@ -8,12 +8,12 @@ using UnityEngine.UI;
 /// The onClick is invoked if back button on Android is pressed.
 /// </summary>
 public class BackButton : Button {
-	#if UNITY_ANDROID
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			onClick.Invoke ();
-		}
-	}
-	#endif
+    #if UNITY_ANDROID
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            onClick.Invoke();
+        }
+    }
+    #endif
 }
